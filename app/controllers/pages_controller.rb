654600727e7ec520b7da7460e845dbc56a73422c
Seb Brooks <sbrookstree@gmail.com>
2020-11-06 14:02:54 +1000

    Added Rolify, created user and buyer roles, set restriction to admin page for non-admins, set default role as buyer

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,18 @@
 class PagesController < ApplicationController
+  before_action :check_roles, only: [:admin]
+
   def index
   end
 
   def admin
+  end
+
+  private
+
+  def check_roles
+    if !current_user.has_role?(:admin)
+      flash[:alert] = "You do not have access to that part of the site"
+      redirect_to root_path
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,15 @@
 class User < ApplicationRecord
+  rolify
+
+  after_create :assign_default_role
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :timeoutable
+  
+  def assign_default_role
+    self.add_role(:buyer)
+  end
 end
