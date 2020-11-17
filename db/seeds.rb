abde936ed4cf8e58459d42b42147062bc34094d1
Seb Brooks <sbrookstree@gmail.com>
2020-11-18 09:53:40 +1000

    updated seed file & removed an empty row in _genres partial

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,10 +18,10 @@ User.find(1).add_role(:artist)
 
 artists = [
     {
-        name: "Aphelion",
+        name: "Dunn Dore",
         description: "I made sounds go bleep",
-        email: "aphelion@music.com",
-        website: "https://www.aphelion.com",
+        email: "dunndore@music.com",
+        website: "https://sound-vision.herokuapp.com/",
         user_id: User.first.id,
         image: {
             io: File.open("app/assets/images/artist.png"),
@@ -32,9 +32,9 @@ artists = [
     },
     {
         name: "Calcium",
-        description: "Sonic sleeplessness",
+        description: "Sonic sleepless but sonic is farst",
         email: "calcium@music.com",
-        website: "https://www..com",
+        website: "https://sound-vision.herokuapp.com/",
         user_id: User.first.id,
         image: {
             io: File.open("app/assets/images/artist.png"),
@@ -47,7 +47,7 @@ artists = [
         name: "Cheese",
         description: "Tastes nice, is nice",
         email: "cheese@music.com",
-        website: "https://www.cheese.com",
+        website: "https://sound-vision.herokuapp.com/",
         user_id: User.first.id,
         image: {
             io: File.open("app/assets/images/artist.png"),
@@ -58,9 +58,9 @@ artists = [
     },
     {
         name: "Eye Lands",
-        description: "Angry rah rah",
+        description: "Angry rah rah music",
         email: "eyelands@music.com",
-        website: "https://www.eyelands.com",
+        website: "https://sound-vision.herokuapp.com/",
         user_id: User.first.id,
         image: {
             io: File.open("app/assets/images/artist.png"),
@@ -71,9 +71,9 @@ artists = [
     },
     {
         name: "Ghost Hurter",
-        description: "London based musician",
+        description: "Spooky scary songs that spagett you",
         email: "ghosthurter@music.com",
-        website: "https://www.ghosthurter.com",
+        website: "https://sound-vision.herokuapp.com/",
         user_id: User.first.id,
         image: {
             io: File.open("app/assets/images/artist.png"),
@@ -86,7 +86,7 @@ artists = [
         name: "Henry Plum",
         description: "Trio from the east side",
         email: "henryplum@music.com",
-        website: "https://www.henryplum.com",
+        website: "https://sound-vision.herokuapp.com/",
         user_id: User.first.id,
         image: {
             io: File.open("app/assets/images/artist.png"),
@@ -99,7 +99,7 @@ artists = [
         name: "Mucor",
         description: "Soundscapes that go bloop diddy wah",
         email: "mucor@music.com",
-        website: "https://www.mucor.com",
+        website: "https://sound-vision.herokuapp.com/",
         user_id: User.first.id,
         image: {
             io: File.open("app/assets/images/artist.png"),
@@ -109,10 +109,10 @@ artists = [
         }
     },
     {
-        name: "Observer",
+        name: "Peep Peep Toot Toot",
         description: "I watch and see everything",
         email: "observer@music.com",
-        website: "https://www.observer.com",
+        website: "https://sound-vision.herokuapp.com/",
         user_id: User.first.id,
         image: {
             io: File.open("app/assets/images/artist.png"),
@@ -122,10 +122,10 @@ artists = [
         }
     },
     {
-        name: "Rhubarb",
+        name: "Duck Guy",
         description: "I live in my house!",
         email: "rhubarb@music.com",
-        website: "https://www.rhubarb.com",
+        website: "https://sound-vision.herokuapp.com/",
         user_id: User.first.id,
         image: {
             io: File.open("app/assets/images/artist.png"),
@@ -138,7 +138,7 @@ artists = [
         name: "Teeth",
         description: "Fun to eat",
         email: "teeth@music.com",
-        website: "https://www.teeth.com",
+        website: "https://sound-vision.herokuapp.com/",
         user_id: User.first.id,
         image: {
             io: File.open("app/assets/images/artist.png"),
@@ -201,8 +201,8 @@ Genre.create!(genres)
 
 songs = [
     {
-        name: "'Neath a Protein Moon",
-        description: "A fun track feat. Justin Case on theremin",
+        name: "Protein Moon",
+        description: "A hardcore techno song that sounds like you wish you were dead",
         price: 2.00,
         exclusive_price: 10.00,
         purchases: 0,
@@ -216,10 +216,10 @@ songs = [
         }
     },
     {
-        name: "In a Protein Sky",
-        description: "A fun track feat. Justin Case on theremin",
-        price: 2.00,
-        exclusive_price: 10.00,
+        name: "Pro Teen Sky",
+        description: "Broken machinery and you're stuck on a bus",
+        price: 3.50,
+        exclusive_price: 999.00,
         purchases: 0,
         available: true,
         artist_id: 2,
@@ -231,10 +231,10 @@ songs = [
         }
     },
     {
-        name: "Running Protein Fields",
-        description: "A fun track feat. Justin Case on theremin",
-        price: 2.00,
-        exclusive_price: 10.00,
+        name: "Running Fields",
+        description: "Like water torture but fun!",
+        price: 0.50,
+        exclusive_price: 666.00,
         purchases: 0,
         available: true,
         artist_id: 3,
@@ -246,10 +246,10 @@ songs = [
         }
     },
     {
-        name: "With My Protein Eye",
-        description: "A fun track feat. Justin Case on theremin",
-        price: 2.00,
-        exclusive_price: 10.00,
+        name: "My Eye",
+        description: "A theme of disco and punk",
+        price: 10.00,
+        exclusive_price: 10000.00,
         purchases: 0,
         available: true,
         artist_id: 4,
@@ -261,10 +261,10 @@ songs = [
         }
     },
     {
-        name: "There's a Protein Song",
-        description: "A fun track feat. Justin Case on theremin",
-        price: 2.00,
-        exclusive_price: 10.00,
+        name: "I Do This Every Night",
+        description: "Babaloo please don't you laugh doc",
+        price: 4.20,
+        exclusive_price: 420.00,
         purchases: 0,
         available: true,
         artist_id: 5,
@@ -276,10 +276,10 @@ songs = [
         }
     },
     {
-        name: "Howling Through Meat",
-        description: "A fun track feat. Justin Case on theremin",
-        price: 2.00,
-        exclusive_price: 10.00,
+        name: "Howling Meat",
+        description: "A fun track feat. the reanimated corpse of Jeff Buckley",
+        price: 7.00,
+        exclusive_price: 67.00,
         purchases: 0,
         available: true,
         artist_id: 6,
@@ -291,10 +291,10 @@ songs = [
         }
     },
     {
-        name: "Driving Protein Bods",
-        description: "A fun track feat. Justin Case on theremin",
-        price: 2.00,
-        exclusive_price: 10.00,
+        name: "Driving Bonds",
+        description: "The interstices of fun and things that taste grey",
+        price: 9.00,
+        exclusive_price: 27.50,
         purchases: 0,
         available: true,
         artist_id: 7,
@@ -306,9 +306,9 @@ songs = [
         }
     },
     {
-        name: "From My Protein Street",
-        description: "A fun track feat. Justin Case on theremin",
-        price: 2.00,
+        name: "From My Street",
+        description: "All the powder from a trumpet Gabriel",
+        price: 10.00,
         exclusive_price: 10.00,
         purchases: 0,
         available: true,
@@ -322,9 +322,9 @@ songs = [
     },
     {
         name: "Pain Is Not Alone",
-        description: "A fun track feat. Justin Case on theremin",
-        price: 2.00,
-        exclusive_price: 10.00,
+        description: "Flush hard, it's a long way to Athens",
+        price: 8.00,
+        exclusive_price: 80.00,
         purchases: 0,
         available: true,
         artist_id: 9,
@@ -337,9 +337,9 @@ songs = [
     },
     {
         name: "Here's to a Lousy Life",
-        description: "A fun track feat. Justin Case on theremin",
-        price: 2.00,
-        exclusive_price: 10.00,
+        description: "Dead in the water",
+        price: 88.00,
+        exclusive_price: 888.00,
         purchases: 0,
         available: true,
         artist_id: 10,
@@ -359,22 +359,42 @@ Song.create!(songs)
 song_genres = [
     {song_id: 1, genre_id: rand(1..41)},
     {song_id: 1, genre_id: rand(1..41)},
+    {song_id: 1, genre_id: rand(1..41)},
+    {song_id: 1, genre_id: rand(1..41)},
+    {song_id: 2, genre_id: rand(1..41)},
+    {song_id: 2, genre_id: rand(1..41)},
     {song_id: 2, genre_id: rand(1..41)},
     {song_id: 2, genre_id: rand(1..41)},
     {song_id: 3, genre_id: rand(1..41)},
     {song_id: 3, genre_id: rand(1..41)},
+    {song_id: 3, genre_id: rand(1..41)},
+    {song_id: 3, genre_id: rand(1..41)},
+    {song_id: 4, genre_id: rand(1..41)},
+    {song_id: 4, genre_id: rand(1..41)},
     {song_id: 4, genre_id: rand(1..41)},
     {song_id: 4, genre_id: rand(1..41)},
     {song_id: 5, genre_id: rand(1..41)},
     {song_id: 5, genre_id: rand(1..41)},
+    {song_id: 5, genre_id: rand(1..41)},
+    {song_id: 5, genre_id: rand(1..41)},
+    {song_id: 6, genre_id: rand(1..41)},
+    {song_id: 6, genre_id: rand(1..41)},
     {song_id: 6, genre_id: rand(1..41)},
     {song_id: 6, genre_id: rand(1..41)},
     {song_id: 7, genre_id: rand(1..41)},
     {song_id: 7, genre_id: rand(1..41)},
+    {song_id: 7, genre_id: rand(1..41)},
+    {song_id: 7, genre_id: rand(1..41)},
+    {song_id: 8, genre_id: rand(1..41)},
+    {song_id: 8, genre_id: rand(1..41)},
     {song_id: 8, genre_id: rand(1..41)},
     {song_id: 8, genre_id: rand(1..41)},
     {song_id: 9, genre_id: rand(1..41)},
     {song_id: 9, genre_id: rand(1..41)},
+    {song_id: 9, genre_id: rand(1..41)},
+    {song_id: 9, genre_id: rand(1..41)},
+    {song_id: 10, genre_id: rand(1..41)},
+    {song_id: 10, genre_id: rand(1..41)}
     {song_id: 10, genre_id: rand(1..41)},
     {song_id: 10, genre_id: rand(1..41)}
 ]
